0af80cb0130786c41d71802dd4ea8ede1165925e
Sam Brown <jumpbro@hotmail.com>
2022-05-14 17:22:48 +1000

    trying to fix for heroku

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ app.delete('/delete', (request, response) => {
 
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log('Server is running on port: '+PORT)
+    console.log('Server is running on port: '+process.env.PORT)
 })
